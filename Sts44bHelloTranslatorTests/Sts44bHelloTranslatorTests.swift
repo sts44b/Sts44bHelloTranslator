d8b4384c5529147324488f17be9e1f4318dffce3
Seanmichael Stanley <SeanmichaelStanley@seanmichaels-macbook-pro.local>
2015-09-22 00:22:49 -0500

    Initial Commit

---
+++ Sts44bHelloTranslatorTests/Sts44bHelloTranslatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sts44bHelloTranslatorTests.swift
+//  Sts44bHelloTranslatorTests
+//
+//  Created by Seanmichael Stanley on 9/22/15.
+//  Copyright © 2015 Seanmichael Stanley. All rights reserved.
+//
+
+import XCTest
+@testable import Sts44bHelloTranslator
+
+class Sts44bHelloTranslatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
